b0e76d520bd965587cf96477d95dcdf473fa20a3
Kovel Alexander <xanderkovel@gmail.com>
2023-11-26 20:42:57 +0300

    -_- remade on microsoft style

--- src/main.c
+++ src/main.c
@@ -16,40 +16,44 @@
 static const size_t num_threads = 10;
 static const size_t num_items = 100;
 
-void worker(void *arg) {
-  int *val = arg;
-  int old = *val;
+void worker(void* arg)
+{
+	int* val = arg;
+	int old = *val;
 
-  *val += 1000;
-  LOG_INFO("tid=%p, old=%d, val=%d\n", pthread_self(), old, *val);
+	*val += 1000;
+	LOG_INFO("tid=%p, old=%d, val=%d\n", pthread_self(), old, *val);
 
-  if (*val % 2)
-	usleep(100000);
+	if (*val % 2)
+		usleep(100000);
 }
 
-int main(int argc, char **argv) {
-  tpool_t *tm;
-  int *vals;
-  size_t i;
+int main(int argc, char** argv)
+{
+	tpool_t* tm;
+	int* vals;
+	size_t i;
 
-  tm = tpool_create(num_threads);
-  vals = calloc(num_items, sizeof(*vals));
+	tm = tpool_create(num_threads);
+	vals = calloc(num_items, sizeof(*vals));
 
-  for (i = 0; i < num_items; i++) {
-	vals[i] = i;
-	tpool_add_work(tm, worker, vals + i);
-  }
+	for (i = 0; i < num_items; i++)
+	{
+		vals[i] = i;
+		tpool_add_work(tm, worker, vals + i);
+	}
 
-  tpool_wait(tm);
+	tpool_wait(tm);
 
-  for (i = 0; i < num_items; i++) {
-	LOG_TRACE("%d\n", vals[i]);
-  }
+	for (i = 0; i < num_items; i++)
+	{
+		LOG_TRACE("%d\n", vals[i]);
+	}
 
-  free(vals);
-  tpool_destroy(tm);
+	free(vals);
+	tpool_destroy(tm);
 
-  creat_socket(PORT);
+	creat_socket(PORT);
 
-  return 0;
+	return 0;
 }
